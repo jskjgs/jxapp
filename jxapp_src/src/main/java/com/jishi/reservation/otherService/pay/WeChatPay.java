0a0ca9b37200fe8aab3f82663b57b87486b6ac9c
dawntl <liang.xiong@hpx10.com>
2017-12-28 21:12:27 +0800

    bug

--- jxapp_src/src/main/java/com/jishi/reservation/otherService/pay/WeChatPay.java
+++ jxapp_src/src/main/java/com/jishi/reservation/otherService/pay/WeChatPay.java
@@ -124,7 +124,6 @@ public class WeChatPay {
         }
         //判断微信标识是否为成功
         if(resp.get("return_code") == null || resp.get("result_code") == null
-                || resp.get("prepay_id") == null
                 || !"SUCCESS".equals(resp.get("return_code"))
                 || !"SUCCESS".equals(resp.get("result_code"))) {
             log.info(String.valueOf("微信错误提示return_msg：" + resp.get("return_msg")));
